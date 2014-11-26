ef910c5032b09b7a05317514fa6d2687b8363d86
SaghirHassan <saghir_hassan@outlook.com>
2014-11-26 07:00:23 +0500

    Basic codes

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour {
+
+    //speed for ball
+    public float speed = 7.0f;
+	// Use this for initialization
+	void Start () {
+
+        rigidbody2D.velocity = Vector2.one.normalized * speed;
+	}
+
+    float hitFactor (Vector2 ballPos, Vector2 playerPos, float playerHeight)
+    {
+
+        return (ballPos.y - playerPos.y) / playerHeight;
+    }
+
+    void OnCollisionEnter2D (Collision2D col)
+    {
+
+        if(col.gameObject.name == "Player1")
+        {
+            float y = hitFactor(transform.position, col.transform.position, ((BoxCollider2D)col.collider).size.y);
+
+            Vector2 dir = new Vector2(1, y).normalized;
+
+            rigidbody2D.velocity = dir * speed;
+        }
+
+        if (col.gameObject.name == "Player2")
+        {
+            float y = hitFactor(transform.position, col.transform.position, ((BoxCollider2D)col.collider).size.y);
+
+            Vector2 dir = new Vector2(-1, y).normalized;
+
+            rigidbody2D.velocity = dir * speed;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    // up and down keys (to be set in the Inspector)
+    public KeyCode up;
+    public KeyCode down;
+
+	// Use this for initialization
+    void FixedUpdate()
+    {
+        // up key pressed?
+        if (Input.GetKey(up))
+        {
+            transform.Translate(new Vector2(0.0f, 0.1f));
+        }
+
+        // down key pressed?
+        if (Input.GetKey(down))
+        {
+            transform.Translate(new Vector2(0.0f, -0.1f));
+        }
+    }
+}
